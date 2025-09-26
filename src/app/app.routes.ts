09e9a691ce32cf9035724ab5f58c6bda2460d23c
Antonio Gabriel Mendoza <antonio.mendoza@Paybps.local>
2025-09-26 17:06:18 +0800

    route change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => import('./pages/home/home').then(c => c.Home)
+        component: Home,
+        //redirectTo: '/home',
+        //pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: ''
     }
 ];
